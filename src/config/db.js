a2960e42e8463fdcd1e2bcc9c46b13cf96389920
codinghack0810 <codinghack30@gmail.com>
2024-06-27 17:13:51 +1000

    version 1

---
+++ src/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+async function dbConnect() {
+  mongoose
+    .connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => {
+      console.log("🆗 MongoDB is Connected..");
+    })
+    .catch((error) => {
+      console.log("🚫 MongoDB is NOT connected..");
+      console.error(error);
+    });
+}
+
+module.exports = dbConnect;
